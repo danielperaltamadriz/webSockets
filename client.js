9f65a771c6763fc07c4dbc8d19f121a38f70cb7d
danielperaltamadriz <danielperaltamadriz@gmail.com>
2016-11-18 09:27:49 -0600

    first commit

---
+++ client.js
@@ -0,0 +1,51 @@
+(function() {
+    'use strict'
+    // var connection = new WebSocket('ws://163.178.104.105:9002');
+    var connection = new WebSocket('ws://127.0.0.1:9002');
+    var mouse = {x: 0, y: 0};
+    var canvas = document.getElementById('drawing');
+    var context = canvas.getContext('2d');
+
+    connection.onopen = function () {
+        console.log((new Date()) + ' Connection  accepted.');
+    };
+
+    connection.onmessage = function (message) {
+        try {
+            var json = JSON.parse(message.data);
+            mouse.x = json.x
+            mouse.y = json.y;
+        } catch (ev) {
+            console.log('This doesn\'t look like a valid JSON: ', message.data);
+            return;
+        }
+        var rect = canvas.getBoundingClientRect();
+        context.fillStyle = "red";
+        context.fillRect(mouse.x, mouse.y, 1, 1);
+        context.stroke();
+    };
+
+    canvas.addEventListener('mousedown', function (e) {
+        var rect = canvas.getBoundingClientRect();
+        mouse.x = e.pageX - rect.left;
+        mouse.y = e.pageY - rect.top;
+        paintCanvas();
+        canvas.addEventListener('mousemove', paintCanvas, false);
+    }, false);
+
+    canvas.addEventListener('mouseup', function (e) {
+        canvas.removeEventListener('mousemove', paintCanvas, false);
+    }, false);
+
+    canvas.addEventListener('mousemove', function (e) {
+        var rect = canvas.getBoundingClientRect();
+        mouse.x = e.pageX - rect.left;
+        mouse.y = e.pageY - rect.top;
+    }, false);
+
+    function paintCanvas() {
+        var json = JSON.stringify(mouse);
+        console.log(json);
+        connection.send(json);
+    };
+})();
---
+++ server.js
@@ -0,0 +1,36 @@
+(function() {
+    'use strict'
+
+    var webSocketsServerPort = 9002;
+    var webSocketServer = require('websocket').server;
+    var http = require('http');
+    var clients = [];
+
+    var server = http.createServer(function (request, response) {
+    });
+
+    server.listen(webSocketsServerPort, function () {
+        console.log((new Date()) + " Server is listening on port: " + webSocketsServerPort);
+    });
+
+    var wsServer = new webSocketServer({
+        httpServer: server
+    });
+
+    wsServer.on('request', function (request) {
+        var connection = request.accept(null, request.origin);
+        console.log((new Date()) + ' Connection accepted.');
+        clients.push(connection);
+
+        connection.on('message', function (msg) {
+            for (var i = 0; i < clients.length; i++) {
+                broadcast(msg.utf8Data);
+            }
+        });
+    });
+
+    function broadcast(msg) {
+        for (var i = 0; i < clients.length; i++)
+            clients[i].send(msg);
+    };
+})();
